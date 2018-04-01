85548f6b4908df3ec7b9493e382de982813ad0bd
Anastasiya <ovchinnickova.anast@gmail.com>
2018-04-01 17:29:24 +0500

    Update main.js

--- main.js
+++ main.js
@@ -73,22 +73,28 @@ function CheckAnswer(){//d N проверки
                                                 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 
                                                 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
                                                 'Э', 'Ю', 'Я'];
-    var ip=document.getElementById('q');
-    var ip2 = ip.value.toString();
-    var d = document.getElementById('d').value;
-    var d2 = parseInt(d);
-    var N = document.getElementById('N').value;
-    var N2 = parseInt(N);
-    var answer=document.getElementById('e').value;
-    var answer2 = answer.split();
-    var arr2  =  [];
-     for( var p = 0, j=0; p < answer2.length; ++p) {
-         arr2[j] = (answer2[p]^d2)%N2; 
-         
-     }
-     var itog = arr2.toString();
-     if (itog == "10" &&  ip2 == "192.168.0.4" )
-     alert('Right! Good job!');
-     else alert("Wrong answer");
- }
- 
+  var ip=document.getElementById('q');
+   var ip2 = ip.value.toString();
+   var d = document.getElementById('d').value;
+   var d2 = parseInt(d);
+   var N = document.getElementById('N').value;
+   var N2 = parseInt(N);
+   var answer=document.getElementById('e').value;
+   var answer2 = Array.from(answer);
+   var dd = "false";
+   dd = IsTheNumberSimple(d2); 
+   var right = ["14", "10", "18", "16", "14"];
+   if (dd==true && N2==10){
+      for( var p = 0, j=0; p < answer2.length; ++p, j++) {
+		 right[j] = (Math.pow(right[p], d2))%N2;
+        // alert(right[j]);
+         }  
+      }
+      else alert("Неправильные данные!");
+    var right = right.toString();
+    var itog = answer2.toString();
+    if (right==itog &&  ip2 == "192.168.0.4" )
+    alert('Right! Good job!');
+    else alert("Wrong answer");
+}
+
