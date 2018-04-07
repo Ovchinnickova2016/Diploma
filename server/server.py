30e04d306f4dea4682231958eaed04f2a5cecbd4
anonimD08 <asdebug@yandex.ru>
2018-04-07 23:54:17 +0500

    Алекандр

---
+++ server/server.py
@@ -0,0 +1,16 @@
+
+from flask import Flask,jsonify,request
+import json
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+CORS(app)
+
+rect = json.load(open('templates/test.json'))
+
+@app.route('/', methods=['GET','POST'])
+def get_tasks():
+    #value = request.json['rect']
+    return jsonify({'array': rect})
+
+if __name__ == '__main__':
+    app.run(debug=True)
