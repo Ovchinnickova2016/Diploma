6e1c4b53d39b31bc52f9d50ccef49f2810161483
Anastasiya <ovchinnickova.anast@gmail.com>
2018-04-10 21:58:39 +0500

    Update Lab1.py

--- server/labs/Lab1.py
+++ server/labs/Lab1.py
@@ -9,15 +9,19 @@ def CheckAnswer(json):
     ch = ['#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ','Э', 'Ю', 'Я']
     dd = 'false'
     dd = IsTheNumberSimple(d2)
-    right = ["14", "10", "18", "16", "14"]
-    if dd==true && N2==10:
-        for int p = 0, j=0; p < answer.length; ++p, j++:
-		    right[j] = (right[p] ** d2)%N2
+    right = ['14', '10', '18', '16', '14']
+    p = 0
+    j = 0
+    k = answer.length
+    if dd==true & N2==10:
+        for p in range (k) :
+            for j in range(k): #int p = 0, j=0, p < answer.length, p=+1, j=+1:
+		        right[j] = right[p] ** d2%N2
     else:
         print("Неправильные данные!")
     right = str(right)
     itog = str(answer2)
-    if right==itog &&  ip2 == "192.168.0.4":
+    if right==itog &  ip2 == "192.168.0.4":
         print('Right! Good job!')
     else:
         print("Wrong answer")
@@ -29,8 +33,9 @@ def IsTheNumberSimple(n):
         return false
     if n == 2:
         return true
-    for int i = 2 in n:
+    i = 2
+    for i in range(n):
         if n % i == 0:
             return false
-        i++
+        i=+1
         return true 
