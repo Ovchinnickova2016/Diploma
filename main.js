025f95ec47948cdee6fa25652150d38311d20696
anonimD08 <asdebug@yandex.ru>
2018-04-14 15:32:14 +0500

    добавление class Start,Rect1; изменение json

--- main.js
+++ main.js
@@ -1,216 +1,77 @@
 
-
-function WorkingSpace(typeobj,x,y,width,height,color,id,mysvg)
-{
-    var newElement = document.createElementNS("http://www.w3.org/2000/svg", typeobj);
-    newElement.setAttribute('x',x);
-    newElement.setAttribute('y',y);
-    newElement.setAttribute('width',width);
-    newElement.setAttribute('height',height);
-    newElement.setAttribute('fill',color);
-    newElement.setAttribute('id',id);
-    mysvg.appendChild(newElement);
-}
-
-function ProtoObject(typeobj,test,mysvg)
-{
-    var newElement = document.createElementNS("http://www.w3.org/2000/svg", typeobj);
-    newElement.setAttributeNS(null,'x',test.x);
-    newElement.setAttributeNS(null,'y',test.y);
-    newElement.setAttributeNS(null,'width',test.width);
-    newElement.setAttributeNS(null,'height',test.height);
-    newElement.setAttributeNS(null,'fill',test.fill);
-    mysvg.appendChild(newElement);
-}
-
 //автоматически вызываемая функция создаёт пространство
 //рабочей области + формирует создание области настроек 
 //и кнопки отправки на проверку. 
+class Start {
+    constructor(){
+        }
+        createElementSVG(name, attributes){
+            this.NS = "http://www.w3.org/2000/svg";
+            this.element = document.createElementNS(this.NS, name);
+            if(name==="svg")
+            {
+                document.getElementById("widget").appendChild(this.element);
+            }
+             if (attributes){
+                 for (name in attributes){;
+                    this.element.setAttributeNS(null,[name],attributes[name]);
+                 }
+            return this.element;
+        }
+    }
+    appendNode(element){
+           return document.querySelector('svg').appendChild(element);
+    }
+}
+
+class Rect1 extends Start{
+    constructor(){
+        super()
+        this.defaultSet ={
+            x:0,
+            y:0,
+            width:850,
+            height:850,
+            fill:'#f5f5f5',
+        }
+        this.constract = (name) => {
+           return this.createElementSVG(name,this.defaultSet);
+        }
+    }
+    Connection(){
+        var fetchResult = fetch('http://localhost:5005/', {mode: 'cors',method: 'get',dataType: 'json'});
+        async function fetchAsync () {
+            var response = await fetchResult;
+            var data = await response.json();
+            return data;
+          }
+          fetchAsync().then(data => {
+             var jsonObj = data.array.Rect1;
+             var amount = Object.keys(jsonObj).length;
+             for(amount in jsonObj)
+             {
+                 console.log(jsonObj[amount].type);
+                 this.appendNode(this.createElementSVG(jsonObj[amount].type,jsonObj[amount]));
+             }
+          })
+    }
+}
+
+class Rect2 extends Start{
+    constructor(){
+        super();
+    }
+
+}
+
  (function(){
-    
-    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    WorkingSpace('rect',0,0,850,850,"#f5f5f5",1,svg);
-    CreateLabSettings1(svg);
-    Connection(svg);
+    var start = new Start();
+    var rect = new Rect1();
+    var svg = start.createElementSVG('svg');
+    start.appendNode(rect.constract('rect'));
+    start.appendNode(rect.constract('image'));
+    rect.Connection();
 }());
-
-function Connection(svg){
-    var fetchResult = fetch('http://localhost:5000/', {mode: 'cors',method: 'get', dataType: 'json'});
-
-    fetchResult.then(function(response) {  
-        return response.json();  
-    }).then(function(json) {  
-        var jsonObj = json.array[0].rect;
-        console.log(jsonObj);
-        console.log(JSON.stringify(jsonObj));
-        ProtoObject('rect',jsonObj,svg);
-    });
-}
-
-function CreateLabSettings1(svg){
-    //создаём тэг svg в тегэ <div id="widget"></div> 
-    document.getElementById("widget").appendChild(svg);
-    //переменная Gimage для картинок в svg
-   // var GImage = document.createElement('svg');
-   // document.getElementById("widget").appendChild(GImage);
-//    var r = getNode('rect', { x: 10, y: 10, width: 100, height: 100, fill:'#ff00ff',href:'images/cloud.svg' });
-//     svg.appendChild(r);
-
-//    ImageCreate(100,100,100,100,"cloudSvg",svg,"images/cloud.svg");
-    //переменная divAction создаёт тэг div c классом action
-    var divAction = document.createElement('div');
-    divAction.className = "action";
-    document.getElementById("widget").appendChild(divAction)
-
-    //переменная in1,in2,in3,in4 создаёт инпут в <div class="action"></div>
-    InputCreate("one", "0.0.0.0", "ip", divAction);
-    InputCreate("two", "d (число)", "d", divAction);
-    InputCreate("two", "N (число)", "N", divAction);
-    InputCreate("three", "Численный ответ", "e", divAction);
-    //output
-    //OutputCreate("one", 10, 2, "out", divAction);
-  //  var table = document.createElementNS('table');
-   // divAction.appendChild(table);
-    
-   // переменная cheсk создаёт кнопку для функции CheckAnswer()
-    ButtonCreate("Check", "Отправить на проверку", 'CheckAnswer();', divAction);
-    ImageCreate(160, 500, 100, 100, '#11111', 1, svg, "images/monitor.svg");
-    ImageCreate(350, 60, 100, 100, '#00000', 2, svg, "images/transfer.svg");
-    ImageCreate(100, 300, 100, 100, '#00000', 3, svg, "images/server-3.svg");
-    ImageCreate(370, 500, 100, 100, '#00000', 4, svg, "images/monitor.svg");
-    ImageCreate(350, 300, 100, 100, '#00000', 5, svg, "images/wifi.svg");
-    ImageCreate(580, 500, 100, 100, '#00000', 6, svg, "images/monitor.svg");
-    LineCreate(190, 350, 350, 350, 4, 'black', 7, svg); //между коммутатором и бд
-    LineCreate(400, 150, 400, 330, 4, 'black', 8, svg);//между коммутатором и облаком
-    LineCreate(360, 370, 220, 510, 4, 'black', 9, svg);//между 1 компом и коммутатором
-    LineCreate(400, 370, 400, 510, 4, 'black', 10, svg);//между 2 компом и коммутатором
-    LineCreate(440, 370, 600, 510, 4, 'black', 11, svg);//между 3 компом и коммутатором
-    ImageCreate(450, 100, 60, 60, '#00000', 12, svg, "images/file.svg");
-
-}
-
-function ButtonCreate(className, textContent, onclick, divAction){
-    var button=document.createElement('button');
-    divAction.appendChild(button);
-    button.setAttribute("class", className);
-    button.innerHTML = textContent;
-    button.setAttribute("onclick", onclick);
-}
-
-//функция для создания инпутов
-function InputCreate(className, placeholder, id, divAction){
-    var input=document.createElement('input');
-    divAction.appendChild(input);
-    input.setAttribute("class", className);
-    input.setAttribute("placeholder", placeholder);
-    input.setAttribute("id", id);
-}
-
-//функция для создания аутпутов
-function OutputCreate(className, rows, cols, id, divAction){
-    var output=document.createElement('textarea');
-    divAction.appendChild(output);
-    output.setAttribute("name", className);
-    output.setAttribute("id", id);
-    output.setAttribute("rows", rows);
-    output.setAttribute("cols", cols);
-}
-//функция для создания изображений
-function ImageCreate(x,y,width,height,color,id,mysvg,href)
-{
-    var img = document.createElementNS("http://www.w3.org/2000/svg",'image');
-    img.setAttributeNS(null,"x",x);
-    img.setAttributeNS(null,"y",y);
-    img.setAttributeNS(null,"width",width);
-    img.setAttributeNS(null,"height",height);
-    img.setAttributeNS(null,"visibility","visible");
-    img.setAttributeNS(null,"color",color);
-    img.setAttributeNS(null,"id",id);
-    img.setAttributeNS("http://www.w3.org/1999/xlink",'href',href);
-    mysvg.appendChild(img);
-}
-
-//функция для создания соединительных линий
-function LineCreate(x1,y1,x2,y2, width, stroke,id,mysvg)
-{
-var line= document.createElementNS("http://www.w3.org/2000/svg",'line');
-line.setAttribute("x1",x1);
-line.setAttribute("y1",y1);
-line.setAttribute("x2",x2);
-line.setAttribute("y2",y2);
-line.setAttribute("stroke-width",width);
-line.setAttribute("stroke",stroke);
-line.setAttribute("id",id);
-mysvg.appendChild(line);
-
-}
-
-//Компьютер
-function Computer() {
-    this.Title = "Компьютер";
-    this.TypeId = "Co";
-    this.Settings = [
-        {
-            Title: "Comp1",
-            IP: "192.168.0.2",
-            light: "red" 
-        },
-        {
-            Title: "Comp2",
-            IP: "192.168.0.3",
-            light: "red" 
-        },
-        {
-            Title: "Comp3",
-            IP: "192.168.0.4",
-            light: "red" 
-        },
-        {
-            Title: "Comp4",
-            IP: "192.168.0.5",
-            light: "green" 
-        }
-    ];
-};
-
-//База данных
-function Database() {
-    this.Title = "База данных";
-    this.TypeId = "DB";
-    this.Settings = [
-        {
-            Title: "DB",
-            IP: "192.168.0.6",
-            light: "black" 
-        }
-    ];
-};
-
-//Облако
-function Cloud() {
-    this.Title = "Облако";
-    this.TypeId = "Cl";
-    this.Settings = [
-        {
-            Title: "Cloud",
-            IP: "192.168.0.1",
-            light: "black" 
-        }
-    ];
-};
-
-//File
-function File() {
-    this.Title = "Файл";
-    this.TypeId = "Fi";
-    this.Settings = [
-        {
-            ClosedKey: "9,5",
-            Message: "406 9915660 05464616061 - 9915660",
-            Access_rights: "192.168.0.5" 
-        }
-    ];
-}; 
 
 //проверка ответа, в будущем проверка будет 
 //производиться на стороне сервера =)
