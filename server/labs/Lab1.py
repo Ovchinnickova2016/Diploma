835958f0bd0fea74abf65d42dd73a98efcebfdf1
Anastasiya <ovchinnickova.anast@gmail.com>
2018-04-15 19:22:26 +0500

    Lab1.py

--- server/labs/Lab1.py
+++ server/labs/Lab1.py
@@ -3,28 +3,30 @@ import json
 #функция проверки ответа
 def CheckAnswer(json):
     json  
-    #d2 
-    #N2
-    #answer
+    ip = "192.168.0.4"
+    d2 = 3
+    N2 = 10
+    answer = 40264
+    answer2 = str(answer)
     ch = ['#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ','Э', 'Ю', 'Я']
-    dd = 'false'
-    #dd = IsTheNumberSimple(d2)
+    dd = False
+    dd = IsTheNumberSimple(d2)
     right = ['14', '10', '18', '16', '14']
     p = 0
     j = 0
-    #k = answer.length
-    #if dd==true & N2==10:
-     #   for p in range (k) :
+    k = len(answer)
+    if dd==True & N2==10:
+        for p,j in range (k) :
      #       for j in range(k): #int p = 0, j=0, p < answer.length, p=+1, j=+1:
-#		        right[j] = (right[p] ** d2)%N2
-   # else:
-    print("Неправильные данные!")
+      	    right[j] = (right[p] ** d2)%N2
+    else:
+        print("Неправильные данные!")
     right = str(right)
-    #itog = str(answer2)
-    #if right==itog &  ip2 == "192.168.0.4":
-    print('Right! Good job!')
-    #else:
-    print("Wrong answer")
+    itog = str(answer2)
+    if right==itog &  ip == "192.168.0.4":
+        print('Right! Good job!')
+    else:
+        print("Wrong answer")
 
 
 #проверка является ли число простым.
