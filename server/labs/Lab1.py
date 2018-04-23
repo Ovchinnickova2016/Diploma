014bc413ca2f2a9d8ec7d8d74997437db172ee92
Anastasiya <ovchinnickova.anast@gmail.com>
2018-04-23 21:46:09 +0500

    Изменила lab1.py и названия компов

--- server/labs/Lab1.py
+++ server/labs/Lab1.py
@@ -1,40 +1,46 @@
 import json
-def checkAnswer():
-    json  
-    ip = "192.168.0.4"
-    ip2 = str(ip)
-    d2 = 3
-    N2 = 10
-    answer0 = 40264
-    answer = [4, 0, 2, 6, 4]
-    answer2 = str(answer)
-    print(answer2)
-    dd = False
+
+student_answer = json.dumps({
+"ip": "192.168.0.4",
+"d2": 3,
+"N2": 10,
+"answer0": 40264
+})
+
+
+def checkAnswer(student_answer):
+    student_answer = json.loads(student_answer)
+    ip = student_answer["ip"]
+    d2 = student_answer["d2"]
+    N2 = student_answer["N2"]
+    answer0 = student_answer["answer0"]
+
+    ip2 = ip
+
+    answer2 = [int(r) for r in list(str(answer0))]
+# print(answer2)
     dd = IsTheNumberSimple(d2)
-    print(dd)
+# print(dd)
     right = [14, 10, 18, 16, 14]
     p = 0
     j = 0
     k = len(str(answer0))
-    print(k)
-    print(N2)
-    if dd==True:
-        if N2==10:
+# print(k)
+# print(N2)
+    if dd is True:
+        if N2 == 10:
             while j < k:
-      	        right[j] = right[j] ** d2 % N2
-      	        print (right[j])
-      	        j+=1
+                right[j] = right[j] ** d2 % N2
+# print(right[j])
+                j += 1
     else:
         print("wrong")
-    right = str(right)
-    print(right)
-    itog = answer2
-    print(itog)
-    if right==itog:
+    if str(right) == str(answer2):
         if ip2 == "192.168.0.4":
             print('Right! Good job!')
-    else:
-        print("Wrong answer")
+        else:
+            print(right, answer2, ip2)
+            print("Wrong answer")
 
 
 def IsTheNumberSimple(n):
@@ -47,6 +53,7 @@ def IsTheNumberSimple(n):
         if n % 2 == 0:
             return False
         else:
-            return True 
+            return True
 
-checkAnswer()
+
+checkAnswer(student_answer)
